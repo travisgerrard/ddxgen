01bacd274ec945192b3ad75853c5d0d32fcb6a89
Travis Gerrard <travisgerrard@gmail.com>
2023-01-14 21:23:54 -0800

    fixed title

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>DDxGen</title>
       </Head>
       <div className="container">
         <div className="header">
