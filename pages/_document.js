6484141a5293c392d9a9dc59005c8a54ba7fc6d4
Travis Gerrard <travisgerrard@gmail.com>
2023-01-14 21:20:31 -0800

    update metadata

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="DDxGen" key="title" />
+        <meta
+          property="og:description"
+          content="generate differential diagnosis"
+          key="description"
+        />
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://vmimr.s3.us-west-2.amazonaws.com/cjr0zfqxh000m0818d0eh5jhc-79-Screen%20Shot%202023-01-14%20at%209.14.07%20PM.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
@@ -17,5 +21,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
